c7efda3c1828e2fac96b298b732d4bc226fcf433
Luigi Domenico Cecchini Soares <luigisoares97@gmail.com>
2017-12-03 00:44:03 -0200

    02-12-2017 -> Route Tracking -> BroadcastReceiver + RouteTrackingUtils

--- app/src/main/java/pucminas/com/br/rotas/fragments/MyMapFragment.java
+++ app/src/main/java/pucminas/com/br/rotas/fragments/MyMapFragment.java
@@ -2,17 +2,21 @@ package pucminas.com.br.rotas.fragments;
 
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,9 +27,18 @@ import pucminas.com.br.rotas.services.RouteTrackService;
 import pucminas.com.br.rotas.utils.PermissionUtils;
 import pucminas.com.br.rotas.utils.RouteTrackingUtils;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,14 +48,35 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MyMapFragment extends Fragment implements OnMapReadyCallback,
                     GoogleMap.OnMyLocationButtonClickListener {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    public static final String DRAW_ACTION = RouteTrackBroadcastReceiver.class.getPackage()
+            .getName() + ".DRAW_REQUESTED";
+    public static final String KEY_IS_TRACKING = "isTracking";
+    public static final String KEY_LOCATIONS = "locations";
     public static final String TAG = MyMapFragment.class.getName();
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private FloatingActionButton mStartEndBtn;
-    private boolean mPermissionDenied;
     private boolean mIsTracking;
+    private boolean mPermissionDenied;
     private Context mContext;
-    private RouteTrackingUtils mRouteTrackingUtils;
+    private FloatingActionButton mStartEndBtn;
+
+    private ArrayList<LatLng> mLocations;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private GoogleMap mMap;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private RouteTrackBroadcastReceiver mBroadcastReceiver;
+
+    // BroadcastReceiver for route tracking
+    public class RouteTrackBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mLocations = intent.getParcelableArrayListExtra(MyMapFragment.KEY_LOCATIONS);
+            drawRoute();
+        }
+    }
 
     /**
      * Factory method used to create fragment.
@@ -59,7 +93,21 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
         mContext = getContext();
         SharedPreferences sharedPreferences = mContext
                 .getSharedPreferences(mContext.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        mIsTracking = sharedPreferences.getBoolean("isTracking", false);
+        mIsTracking = sharedPreferences.getBoolean(KEY_IS_TRACKING, false);
+
+        mLocations = new ArrayList<>();
+        mFusedLocationProviderClient = RouteTrackingUtils.createFusedLocation(mContext);
+        mLocationRequest = RouteTrackingUtils.createLocationRequest();
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                mCurrentLocation = locationResult.getLastLocation();
+
+                if (mIsTracking) {
+                    moveCamera(mCurrentLocation);
+                }
+            }
+        };
     }
 
     @Override
@@ -87,14 +135,13 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
 
         mStartEndBtn.setOnClickListener((view) -> {
             mIsTracking = !mIsTracking;
-            mRouteTrackingUtils.setmIsTracking(mIsTracking);
 
             changeStartEndBtn();
 
             SharedPreferences sharedPreferences = mContext
                     .getSharedPreferences(mContext.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean("isTracking", mIsTracking);
+            editor.putBoolean(KEY_IS_TRACKING, mIsTracking);
             editor.apply();
 
             if (mIsTracking) {
@@ -103,6 +150,7 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
 
                 // Call intent service.
                 Intent serviceIntent = new Intent(mContext, RouteTrackService.class);
+                serviceIntent.putParcelableArrayListExtra(KEY_LOCATIONS, mLocations);
                 mContext.startService(serviceIntent);
 
             } else {
@@ -121,6 +169,21 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
             showMissingPermissionError();
             mPermissionDenied = false;
         }
+
+        // Register broadcast receiver
+        IntentFilter broadcastFilter = new IntentFilter(DRAW_ACTION);
+        mBroadcastReceiver = new RouteTrackBroadcastReceiver();
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(mContext);
+        localBroadcastManager.registerReceiver(mBroadcastReceiver, broadcastFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Unregister broadcast receiver
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(mContext);
+        localBroadcastManager.unregisterReceiver(mBroadcastReceiver);
     }
 
     @Override
@@ -139,9 +202,10 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.setOnMyLocationButtonClickListener(this);
-        mRouteTrackingUtils = new RouteTrackingUtils(mContext, mIsTracking, googleMap);
-        mRouteTrackingUtils.startLocationUpdates();
+        mMap = googleMap;
+        mMap.setOnMyLocationButtonClickListener(this);
+        RouteTrackingUtils.startLocationUpdates(mContext, mLocationRequest, mLocationCallback,
+                mFusedLocationProviderClient);
 
         if (! PermissionUtils.checkLocationPermission(getContext())) {
             // Permission to access the location is missing.
@@ -150,15 +214,14 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
                     Manifest.permission.ACCESS_FINE_LOCATION, true);
         } else {
             // Access to the location has been granted to the app.
-            googleMap.setMyLocationEnabled(true);
+            mMap.setMyLocationEnabled(true);
 
             // Get current location at the first time map arrived.
-            mRouteTrackingUtils.getmFusedLocationClient().getLastLocation()
-                    .addOnSuccessListener(location ->
-                            mRouteTrackingUtils.setmCurrentLocation(location));
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(location -> mCurrentLocation = location);
 
             // Move camera to current location
-            mRouteTrackingUtils.moveCamera(mRouteTrackingUtils.getmCurrentLocation());
+            moveCamera(mCurrentLocation);
         }
     }
 
@@ -173,7 +236,7 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
         if (PermissionUtils.isPermissionGranted(permissions, grantResults,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
             // Enable the my location layer if the permission has been granted.
-            mRouteTrackingUtils.moveCamera(mRouteTrackingUtils.getmCurrentLocation());
+            moveCamera(mCurrentLocation);
         } else {
             // Display the missing permission error dialog when the fragments resume.
             mPermissionDenied = true;
@@ -186,6 +249,42 @@ public class MyMapFragment extends Fragment implements OnMapReadyCallback,
     private void showMissingPermissionError() {
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getActivity().getSupportFragmentManager(), "dialog");
+    }
+
+    /**
+     * Set up map location.
+     */
+    private void moveCamera(Location location) {
+        if (location != null) {
+            // Get latitude of the current location
+            double latitude = location.getLatitude();
+
+            // Get longitude of the current location
+            double longitude = location.getLongitude();
+
+            // Create a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            // Show the current location in Google Map
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            // Zoom in the Google Map
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+            drawRoute();
+        }
+    }
+
+    private void drawRoute() {
+        PolylineOptions options = new PolylineOptions();
+        options.color(Color.parseColor("#FF0000"));
+        options.width(10);
+
+        for (LatLng coords : mLocations) {
+            options.add(coords);
+        }
+
+        mMap.addPolyline(options);
     }
 
     private void changeStartEndBtn() {
--- app/src/main/java/pucminas/com/br/rotas/services/RouteTrackService.java
+++ app/src/main/java/pucminas/com/br/rotas/services/RouteTrackService.java
@@ -1,17 +1,26 @@
 package pucminas.com.br.rotas.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v4.content.LocalBroadcastManager;
 
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+import pucminas.com.br.rotas.fragments.MyMapFragment;
+import pucminas.com.br.rotas.utils.RouteTrackingUtils;
 
 public class RouteTrackService extends IntentService {
     public static volatile boolean isTracking = true;
-
+    private ArrayList<LatLng> mLocations;
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -24,13 +33,45 @@ public class RouteTrackService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         RouteTrackService.isTracking = true;
+        startTracking();
 
         while (RouteTrackService.isTracking) {
             try {
                 Thread.sleep(5000);
+
+                // Send broadcast to draw route
+                sendBroadcast();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void startTracking() {
+        mLocations = new ArrayList<>();
+
+        FusedLocationProviderClient fusedLocationProviderClient =
+                RouteTrackingUtils.createFusedLocation(this);
+        LocationRequest locationRequest = RouteTrackingUtils.createLocationRequest();
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (RouteTrackService.isTracking) {
+                    Location location = locationResult.getLastLocation();
+                    mLocations.add(new LatLng(location.getLatitude(), location.getLongitude()));
+                }
+            }
+        };
+
+        RouteTrackingUtils.startLocationUpdates(this, locationRequest, locationCallback,
+                fusedLocationProviderClient);
+    }
+
+    private void sendBroadcast() {
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(MyMapFragment.DRAW_ACTION);
+        broadcastIntent.putParcelableArrayListExtra(MyMapFragment.KEY_LOCATIONS, mLocations);
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        localBroadcastManager.sendBroadcast(broadcastIntent);
     }
 }
